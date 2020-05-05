37bb4e455f555bccf1e8ff49b6f18b1680c5c417
Yulia Chornenka <blackyulia583@gmail.com>
2020-05-05 21:45:27 +0300

    Config for Travis

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/TodoList'));
+app.use(express.static(__dirname + '/dist/ToDoList'));
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/TodoList/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ToDoList/index.html'));
 });
 app.listen(process.env.PORT || 8080);
